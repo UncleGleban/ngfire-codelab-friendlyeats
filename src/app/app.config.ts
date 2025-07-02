092aef1369d324f821e20f5b603f0e745a92a5e4
John Thomas Kittredge <jontom@kittredge.com>
2025-07-02 12:19:18 -0400

    Add environment.prod.ts.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -12,13 +12,13 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideFirebaseApp(() =>
       initializeApp({
-        projectId: 'friendlychat-46816',
-        appId: '1:472756664595:web:7cdea8f77590b29e02c2e6',
-        storageBucket: 'friendlychat-46816.firebasestorage.app',
         apiKey: 'AIzaSyDGe1KfHsm9KwrxTsILen7jSMJ7EP60ck4',
+        appId: '1:472756664595:web:7cdea8f77590b29e02c2e6',
         authDomain: 'friendlychat-46816.firebaseapp.com',
-        messagingSenderId: '472756664595',
         measurementId: 'G-ZJ9MRDTWYH',
+        messagingSenderId: '472756664595',
+        projectId: 'friendlychat-46816',
+        storageBucket: 'friendlychat-46816.firebasestorage.app',
       })
     ),
     provideAuth(() => getAuth()),
